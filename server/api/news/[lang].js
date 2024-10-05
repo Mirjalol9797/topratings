b6e1fc7e6f1b806c6bf10bfbc7edaa5a6336601b
Mirjalol <miramirxomitov97@gmail.com>
2024-10-05 14:49:03 +0500

    added id and slug

--- server/api/news/[lang].js
+++ server/api/news/[lang].js
@@ -5,6 +5,8 @@ export default defineEventHandler((event) => {
   const data = {
     en: [
       {
+        id: 1,
+        slug: "new_title",
         title: "New title",
         description: "Description in English",
         image: "/news-img/test.jpg",
@@ -12,6 +14,8 @@ export default defineEventHandler((event) => {
     ],
     ru: [
       {
+        id: 2,
+        slug: "novoye_nazvaniye",
         title: "Новое название",
         description: "Описание на русском",
         image: "/news-img/test.jpg",
@@ -19,6 +23,8 @@ export default defineEventHandler((event) => {
     ],
     uz: [
       {
+        id: 3,
+        slug: "yangi_sarlavha",
         title: "Yangi sarlavha",
         description: "O‘zbek tilidagi ta’rif",
         image: "/news-img/test.jpg",
