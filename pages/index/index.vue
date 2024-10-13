1bebb5adc25b00cc2e892eb05431ef4113aeb2d7
Mirxomitov Mirjalol <miramirxomitov97@gmail.com>
2024-10-13 22:28:11 +0500

    added left column

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -45,7 +45,24 @@ const news = ref(null);
                 Последные
               </div>
             </div>
-            <div></div>
+            <div class="h-[350px] overflow-y-auto pr-1 site-scroll">
+              <nuxt-link
+                to="/"
+                class="flex items-start gap-3 mb-4"
+                v-for="(item, index) in 10"
+                :key="index"
+              >
+                <div class="font-medium text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+                <img
+                  src="https://daryo.uz/static/2024/10/thumb-670bce4513abb.jpg"
+                  alt=""
+                  title=""
+                  class="w-[90px] h-[70px] object-cover"
+                />
+              </nuxt-link>
+            </div>
           </div>
         </div>
       </div>
