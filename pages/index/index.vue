8039831ef02f9b66c9c995d5b6561467a37cd19f
Mirxomitov Mirjalol <miramirxomitov97@gmail.com>
2024-10-13 22:34:19 +0500

    added last 10 block

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,6 +8,7 @@ import CategoryLeftMain from "./modules/CategoryLeftMain.vue";
 import CategoryRightMain from "./modules/CategoryRightMain.vue";
 import CategoryDoubleTopMain from "./modules/CategoryDoubleTopMain.vue";
 import CategoryNotMain from "./modules/CategoryNotMain.vue";
+import CategoryLastTen from "./modules/CategoryLastTen.vue";
 
 const news = ref(null);
 </script>
@@ -37,33 +38,7 @@ const news = ref(null);
         </div>
         <div class="w-[32%] ml-[3%]">
           <!-- last 10 news -->
-          <div>
-            <div class="main-page-category-title-wrap">
-              <div
-                class="bg-[#315efb] font-medium px-2 py-2 rounded-[4px] text-base w-full"
-              >
-                Последные
-              </div>
-            </div>
-            <div class="h-[350px] overflow-y-auto pr-1 site-scroll">
-              <nuxt-link
-                to="/"
-                class="flex items-start gap-3 mb-4"
-                v-for="(item, index) in 10"
-                :key="index"
-              >
-                <div class="font-medium text-sm">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </div>
-                <img
-                  src="https://daryo.uz/static/2024/10/thumb-670bce4513abb.jpg"
-                  alt=""
-                  title=""
-                  class="w-[90px] h-[70px] object-cover"
-                />
-              </nuxt-link>
-            </div>
-          </div>
+          <CategoryLastTen />
         </div>
       </div>
     </div>
---
+++ pages/index/modules/CategoryLastTen.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div class="main-page-category-title-wrap">
+      <div
+        class="bg-[#315efb] font-medium px-2 py-2 rounded-[4px] text-base w-full"
+      >
+        Последные
+      </div>
+    </div>
+    <div class="h-[350px] overflow-y-auto pr-1 site-scroll">
+      <nuxt-link
+        to="/"
+        class="flex items-start gap-3 mb-4"
+        v-for="(item, index) in 10"
+        :key="index"
+      >
+        <div class="font-medium text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+        <img
+          src="https://daryo.uz/static/2024/10/thumb-670bce4513abb.jpg"
+          alt=""
+          title=""
+          class="w-[90px] h-[70px] object-cover"
+        />
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
