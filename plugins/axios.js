8a34dabc8a4d540946834d84ab994a1f0be09281
Mirjalol <miramirxomitov97@gmail.com>
2024-10-21 16:09:35 +0500

    some change in asiols

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,7 +5,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const axiosPlugin = axios.create({});
 
   // axiosPlugin.defaults.baseURL = "https://backend.toprankings.uz/api/";
-  axiosPlugin.defaults.baseURL = "http://127.0.0.1:8000/api/";
+  // axiosPlugin.defaults.baseURL = "http://127.0.0.1:8000/api/";
 
   axiosPlugin.interceptors.request.use(
     (config) => {
