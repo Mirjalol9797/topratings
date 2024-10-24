26e9d1c249a54f77eac8e94b1d86cfd12a7a27d0
Mirjalol <miramirxomitov97@gmail.com>
2024-10-24 14:25:47 +0500

    added google analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,22 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 const settingsStore = useSettingsStore();
+
+if (process.client) {
+  // Асинхронная загрузка gtag.js
+  const script = document.createElement("script");
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-EG248XZFC7";
+  script.async = true;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "G-EG248XZFC7");
+}
 </script>
 
 <template>
