0e3fae14607a489e9de41a62a34770df49269b85
Mirjalol <miramirxomitov97@gmail.com>
2024-10-24 18:14:18 +0500

    change meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,14 +18,6 @@ if (process.client) {
   gtag("js", new Date());
 
   gtag("config", "G-EG248XZFC7");
-
-  // Асинхронная загрузка скрипта Google AdSense
-  const adsenseScript = document.createElement("script");
-  adsenseScript.src =
-    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3990212606361557"; // замените client=11111111 на ваш client ID
-  adsenseScript.async = true;
-  adsenseScript.setAttribute("crossorigin", "anonymous");
-  document.head.appendChild(adsenseScript);
 }
 </script>
 
